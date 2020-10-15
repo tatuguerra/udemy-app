23521148e75776103c4ddacbdd7ffa14eabf07aa
Mateus Guerra <mateus.guerra@nateevo.com>
2020-10-16 00:11:41 +0200

    Add ngmodule to form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'udemy-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('udemy-app');
+    expect(app.name).toEqual('udemy-app');
   });
 
   it('should render title', () => {
